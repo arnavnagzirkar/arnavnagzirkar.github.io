23ab16788a3fdeca8020a28580b313c217c62512
Arnav Nagzirkar <arnav.nagzirkar@mail.utoronto.ca>
2025-10-24 13:06:28 -0400

    figma updated

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ParticleBackground } from './components/ParticleBackground';
 import { Navigation } from './components/Navigation';
 import { Hero } from './components/Hero';
